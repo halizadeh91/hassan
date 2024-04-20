ee55f6826a4524233849db67d57c841a5ef488d0
Hassan <hassanalizadeh91@gmail.com>
2024-04-20 02:04:01 -0500

    add name and details

--- js/script.js
+++ js/script.js
@@ -2,23 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 gsap.registerPlugin(ScrollTrigger);
 
-// const Line1 = document.querySelector('.line1');
-// const Line2 = document.querySelector('.line2');
-// const Line3 = document.querySelector('.line3');
-
-// gsap.to(Line1, {
-//   duration: 2,
-//   x:400,
-//   stagger: 0.5,
-//   scrollTrigger: {
-//     trigger: Line1,
-//     end: "+=300%",
-//     scrub: true,
-//     pin: true
-//   }
-// })
-
-
 gsap.timeline({
   scrollTrigger: {
     trigger: ".grid-container",
@@ -39,13 +22,28 @@ gsap.timeline({
 
 // Images to make it look better, not related to the effect
 const size = Math.max(innerWidth, innerHeight);
-// gsap.set('.gridBlock', {backgroundImage: i => `url(https://picsum.photos/${size}/${size}?random=${i})`});
+gsap.set('.gridBlock', {backgroundImage: i => `url(https://picsum.photos/${size}/${size}?random=${i})`});
 
 const bigImg = new Image;    
 bigImg.addEventListener("load", function () {
   gsap.to(".centerPiece .gridBlock", {autoAlpha: 1, duration: 0.5});
 });
 
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.fromTo('.box', {
+  opacity: .3 // Starting opacity
+}, {
+  opacity: 1, // Ending opacity
+  scrollTrigger: {
+    trigger: ".grid-container",
+    start: 'top top',
+    end: () => innerHeight * 4,
+    scrub: true,
+    toggleActions: 'restart pause reverse pause' // This makes the animation go back and forth
+  }
+});
 
 
 });
